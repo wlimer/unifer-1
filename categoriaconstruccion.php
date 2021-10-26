ccde34744324073322336470cf094eca81b116fb
stiven madrid <esmadrid2@misena.edu.co>
2021-10-26 10:20:12 -0500

    se modifico toda la pagina pricipal en la primeraRama

--- categoriaconstruccion.php
+++ categoriaconstruccion.php
@@ -9,35 +9,49 @@ $ejecutar = $conexion->query($sql);
 
 
 <body background=pink>
-<table border=1  width=100% >
-<tr> <b><h1 align="center"></h1> </b></tr>
-    <tr bgcolor= #ffdf6b> 
-         <td>producto </td>
-        <td>Nombre producto </td>
-    
-        <td> Precio </td>
-        <td> Referencia </td>
-   
-        <td width="10%"> Imagen </td>
-        <td> cantidad </td>
-    </tr>
-    
-<?php
-   while ($fila = mysqli_fetch_object($ejecutar))
-   
-
-    {
-        echo '<tr >';
-        echo '<td bgcolor= #00adb5>'.$fila->id_productos.'</td>';
-       
-        echo '<td bgcolor= #00adb5>'.$fila->nombre_producto.'</td>';
+<link rel="stylesheet" type="text/css" href="css/styles_tablaproductos.css">
+    <main class="separarproductos">
+          <div class="contenedor__productostabla">    
+          <table>
      
-        echo '<td bgcolor= #aad8d3>'.$fila->precio.'</td>';
-        echo '<td bgcolor= #eeeeee>'.$fila->referencia.'</td>';
-    echo '<td bgcolor=#fcecdd><img src="imagen/' .$fila->Imagen.'" width="300" height="150"></td>';
-    echo '<td> <form action="agregar_carrito.php" method="POST"> <input type="hidden" name="id_productos" value="'.$fila->id_productos.'">  <input type="text" name="cantidad" size="4" required>  <input type="submit" value="Agregar"> </form></td>';
-        echo '</tr>';
-       
-    }
-?></body>
-</table>
+            
+        
+      <tr>
+                <td>producto </td>
+           
+                <td>Nombre producto </td>
+            
+                <td> Precio </td>
+                <td> Referencia </td>
+        
+                <td> Imagen </td>
+                <td> cantidad </td>
+            
+            </tr>
+
+            <h1 align="center"><b> BIENVENIDOS A SU TIENDA UNIFER</b> </h1>
+   
+        <?php
+            while ($fila = mysqli_fetch_object($ejecutar))
+            
+
+                {
+                
+                    echo '<tr >';
+                    echo '<td>'.$fila->id_productos.'</td>';
+                
+                    
+                    echo '<td>'.$fila->nombre_producto.'</td>';
+                
+                    echo '<td >'.$fila->precio.'</td>';
+                    echo '<td>'.$fila->referencia.'</td>';
+                echo '<td ><img src="imagen/' .$fila->Imagen.'" width="300" height="150"></td>';
+                echo '<td> <form action="agregar_carrito.php" method="POST"> <input type="hidden" name="id_productos" value="'.$fila->id_productos.'">  <input type="number" name="cantidad" size="4" min="1" max="50" required>  <input type="submit" value="Agregar" class="btn btn-primary btn-block btn-large"> </form></td>';
+                    echo '</tr>';
+                
+                }
+            ?>
+  
+    </main>   
+    </body>
+        </table>
